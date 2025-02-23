8bd1e628d92dec9dc10cb2a6a93fbc1a1668f983
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-23 13:37:42 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -19,3 +19,24 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000); // Creating a ne
 
 console.log(emp1.getDetails()); // Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
 console.log("Annual Salary:", emp1.calculateAnnualSalary()); // Expected output: 60000
+
+// Task 2 Creating a Manager Class 
+
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary); // Call parent employee constructor
+        this.teamSize = teamSize; // Team size of Manager
+    }
+    getDetails() {
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    } // Manager Details
+    calculateBonus() {
+        return this.salary * 12 * 0.10; // Calculation of Annual Manager Bonus
+    }
+};
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5); // Creating new Manager Object
+
+console.log(mgr1.getDetails()); // Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+console.log("Bonus:", mgr1.calculateBonus()); // Expected output: 9600
+
